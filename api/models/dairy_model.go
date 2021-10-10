8e1bbcf856c3b50cc0b2bc6088851bef07df832c
Gwo(1552571230@qq.com) <1552571230@qq.com>
2021-10-10 22:55:42 +0800

    [增加分页返回日记函数]

--- api/models/dairy_model.go
+++ api/models/dairy_model.go
@@ -1,25 +1,48 @@
 package models
 
-import orm "GoToBetterLife/api/database"
+import (
+	orm "GoToBetterLife/api/database"
+	"time"
+)
 
 type Dairy struct {
-	Datetime       string `json:"datetime"`
+	Datetime       int  `json:"datetime"`
 	Article        string `json:"article"`
 	BeOnDutyTime   string `json:"be_on_duty_time"`
 	OffDutyTime    string `json:"off_duty_time"`
-	IsWorkOutToday string `json:"is_work_out_today"`
+	IsWorkOutToday string `json:"is_workout_today"`
 }
 
 var Dairies []Dairy
 
-// GetTenDairies 获取到最近十天的日记
-func (dairy *Dairy) GetTenDairies() (dairies []Dairy,err error){
-	if err=orm.Db.Find(&dairies).Limit(10).Error;err!= nil {
+// GetLatestTenDairies 获取到最近十天的日记，用于页面刚刚加载的时候返回给到前端
+func (dairy *Dairy) GetLatestTenDairies() (dairies []Dairy, err error) {
+	if err = orm.Db.Find(&dairies).Limit(10).Error; err != nil {
 		return
 	}
 	return
 }
 
-func (dairy *Dairy) AddNewDairies(article string,isworkouttoday string)  {
+//GetNewPage 这里需要分页返回每10天的记录
+func (dairy *Dairy) GetNewPage(page int) (dairies []Dairy, err error) {
+	// 这里分别获取到当前的年月日，用来存库
+	year := time.Now().Year()
+	month := int(time.Now().Month())
+	day := time.Now().Day()
+	var nowData = year*10000 + month*100 + day
+	// 这里分别获取到需要返回页的其实日期的数据和结束日期的数据
+	var endDate = nowData - (page-1)*10
+	var startDate = endDate - 10
+
+	err = orm.Db.Where("datetime BETWEEN ? AND ?", endDate, startDate).Find(&dairies).Error
+	if err != nil {
+		return nil, err
+	}
+	return
+}
+
+//InsertNewDairies 用来记录今天的上班下班时间、是否锻炼身体、是否学习等
+func (dairy *Dairy) InsertNewDairies(datetime int64,)(result string) {
+	result := orm.Db.Create()
 
 }
---
+++ test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Print(int(time.Now().Month()))
+}
