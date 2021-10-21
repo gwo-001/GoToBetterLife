673f32f163a843a10edb163412760c7218f55232
Gwo(1552571230@qq.com) <1552571230@qq.com>
2021-10-21 23:37:36 +0800

    [增加token校验方法]

--- api/auth_api.go
+++ api/auth_api.go
@@ -16,19 +16,35 @@ var (
 	ExpireTime = 3600  // 过期时间
 )
 
+const (
+	ServerBusy = "Server busy"
+)
+
 // GetToken 获取token
 func GetToken(claims *JWTClaims) (tokenStr string, err error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
 	tokenStr, err = token.SignedString([]byte(Secret))
 
 	if err != nil {
-		return "",errors.New("Server busy")
+		return "",err
 	}
 	return
 }
 
 // VerifyToken 校验token
 func VerifyToken(tokenStr string)(*JWTClaims,error)  {
-	// todo 增加token校验方法
-	return nil,nil
+	token,err:=jwt.ParseWithClaims(tokenStr,&JWTClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(Secret),nil
+	})
+	if err != nil {
+		return nil, errors.New(ServerBusy)
+	}
+	claims,ok:=token.Claims.(*JWTClaims)
+	if !ok {
+		return nil,errors.New(ServerBusy)
+	}
+	if err:=token.Claims.Valid();err!=nil {
+		return nil, errors.New(ServerBusy)
+	}
+	return claims,nil
 }
